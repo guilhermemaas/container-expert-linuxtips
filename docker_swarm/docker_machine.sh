0a70101ee28604151ea03c3fb287f9971911bb0d
Guilherme Maas <guilherme.maas@gmail.com>
2020-12-22 21:09:24 -0300

    docker-machine e docker swarm.

---
+++ docker_swarm/docker_machine.sh
@@ -0,0 +1,72 @@
+#Machine Drivers:
+#https://docs.docker.com/machine/drivers/
+
+#Instalação do docker machine no Linuxs:
+base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
+  chmod +x /usr/local/bin/docker-machine
+
+  #Versão:
+  docker-machine --version
+  docker-machine version 0.16.0, build 702c267f
+
+Latest version:
+https://github.com/docker/machine/releases
+
+
+#Criando uma nova docker Machine com VirtualBox:
+docker-machine create --driver virtualbox giropops
+
+#Verificar as máquinas:
+docker-machine ls 
+#NAME       ACTIVE   DRIVER       STATE     URL                         SWARM   DOCKER      ERRORS
+#giropops   -        virtualbox   Running   tcp://192.168.99.100:2376           v19.03.12 
+
+#Verificar o IP da máquina, nó:
+docker-machine ip giropops
+
+#Conectar ssh na máquina:
+docker-machine ssh giropops
+
+#Verificar váriáveis do node:
+docker-machine env giropops
+
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://192.168.99.100:2376"
+export DOCKER_CERT_PATH="/home/ilhanublar/.docker/machine/machines/giropops"
+export DOCKER_MACHINE_NAME="giropops"
+# Run this command to configure your shell: 
+# eval $(docker-machine env giropops)
+
+#Para se conectar o CLI a machine, node:
+eval $(docker-machine env NOME_DO_NODE)
+#Basicamente vai dar um export conforme saída do comando docker-machine env giropops
+
+#Estando conectado pode ser criado um container normalmente:
+docker container run -d -p 8080:80 nginx
+#-d = Rodar como daemon, em foregrounbd(Entrypoint)
+#-p 8080 = Porta externa na máquina, node
+#-p 80 = Porta do container #BIND DE PORTA
+
+docker-machine ls
+docker-machine ip giropops
+curl IP:8080
+
+#Parar a instância, node:
+docker-machine stop giropops
+
+#Inicia a instância, node:
+docker-machine start giropops
+
+docker-machine env -u
+ $ docker-machine env -u
+unset DOCKER_TLS_VERIFY
+unset DOCKER_HOST
+unset DOCKER_CERT_PATH
+unset DOCKER_MACHINE_NAME
+# Run this command to configure your shell: 
+# eval $(docker-machine env -u)
+
+#Para remover uma máquina, node:
+docker-machine rm giropops
---
+++ docker_swarm/docker_swarm.sh
@@ -0,0 +1,74 @@
+#Commands:
+docker swarm --help
+ca Display and rotate the root CA 
+init Initialize a swarm
+join Join a sarm as a node and/or manager
+join-token Manage join tokens
+leave Leave the sarm
+unlock Unlock the swarm
+unlock-key Manage the unlock key
+update Update the swarm
+
+#Para iniciar um Cluster:
+docker swarm init
+
+Swarm initialized: current node (6w39ta36uewdyjb7wf60gb4f0) is now a manager.
+
+To add a worker to this swarm, run the following command:
+
+    docker swarm join --token SWMTKN-1-2v7dgp0stninkb3p4mxki465avmgvihisji3h90hu50xvqhk3r-3d7xd1aakxazddyqvu7unaat8 192.168.0.110:2377
+
+To add a manager to this swarm, run 'docker swarm join-token manager' and follow the instructions.
+
+[root@localhost ~]# docker node ls
+ID                            HOSTNAME            STATUS              AVAILABILITY        MANAGER STATUS      ENGINE VERSION
+6w39ta36uewdyjb7wf60gb4f0 *   ilha-nublar01       Ready               Active              Leader              19.03.14
+
+
+#Instalar o docker em outros nós:
+curl -fsSL https://get.docker.com | bash
+
+#Fazer com que um host com docker de um join no cluster swarm:
+docker swarm join --token SWMTKN-1-2v7dgp0stninkb3p4mxki465avmgvihisji3h90hu50xvqhk3r-3d7xd1aakxazddyqvu7unaat8 192.168.0.110:2377
+
+#Listar os nodes do cluster:
+docker node ls
+ID                            HOSTNAME            STATUS              AVAILABILITY        MANAGER STATUS      ENGINE VERSION
+6w39ta36uewdyjb7wf60gb4f0 *   ilha-nublar01       Ready               Active              Leader              19.03.14
+yk6y4t7f11742g6riuisxwy6g     ilha-nublar02       Ready               Active                                  20.10.1
+q0hntx98ode983w5ln1tpaxo4     ilha-nublar03       Ready               Active                                  20.10.1
+
+#Promover um node para Manager:
+docker node promote ilha-nublar02
+
+docker node ls
+
+#Fazer com que um node saiu do Cluster:
+docker swarm leave
+
+#Remover um node a partir do Manager Leader:
+docker node rm ID
+
+#Despromover um node, para deixar de ser manager:
+docker node demote nome ou ID
+
+#Definir o IP na criação do cluster, por onde vai se comunicar:
+docker swarm init --advertise-addr 192.168.0.1
+
+#Regerar o comando de join no Manager, com o token:
+docker swarm join-token worker
+
+
+#Gerar um token para se conectar como manager.
+docker swarm join-token manager
+To add a manager to this swarm, run the following command:
+
+    docker swarm join --token SWMTKN-1-3tdexah0hte1bll306ok0ftbj51zrega1158ekymqkp9m0jk3b-2b754be86p2us9nrq1uvb1oos 192.168.0.110:2377
+
+#Criar um novo token:
+docker swarm join-token --rotate manager
+
+#Matar o node Manager Leader:
+docker swarm leave --force
+#Nesse caso precisa dar init novamente no cluster swarm.
+
